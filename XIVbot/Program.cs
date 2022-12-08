291d130918ba82e455198fff613f672c014b7106
落ちていたビール缶 <67153833+FalledCan@users.noreply.github.com>
2022-12-08 17:33:08 +0900

    market確認

--- XIVbot/Program.cs
+++ XIVbot/Program.cs
@@ -58,9 +58,9 @@ class Program
         var globalCommand = new SlashCommandBuilder()
             .WithName("search")
             .WithDescription("FFXIV内のアイテム検索")
-            .AddOption("itemname", ApplicationCommandOptionType.String, "アイテム名を書いてください",isRequired: true);
+            .AddOption("itemname", ApplicationCommandOptionType.String, "アイテム名を書いてください", isRequired: true);
         try
-        { 
+        {
             await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
         }
         catch (ApplicationCommandException exception)
@@ -92,27 +92,45 @@ class Program
         {
             await command.RespondAsync("正しいアイテム名を入力して下さい。");
         }
-        else
-        {
+        string[] strsplit = str.Split(",");
+        string[] strsplit2 = market_world().Split(",");
 
-            string[] strsplit = str.Split(",");
-            var embedBuilder = new EmbedBuilder()
-                .WithAuthor(strsplit[2], "https://xivapi.com/" + strsplit[1])
-                .WithColor(Color.Green)
-                .WithTitle("マーケット一覧")
-                .WithCurrentTimestamp();
-            await command.RespondAsync(embed: embedBuilder.Build());
+        var list = new List<EmbedFieldBuilder>();
+
+        for(int i = 0; i < 6; i++)
+        list.Add(
+            new EmbedFieldBuilder()
+            {
+                //0,4,8,12,16,20//1,5,9,13,17,21//3,7,11,15,19,23
+                Name = "Ifrit",
+                Value = "単価: " + strsplit2[i == 0 ? 0:i == 1 ? 4:i == 2 ? 8:i == 3 ? 12:i == 4? 16:20] 
+                + " 個数: " + strsplit2[i == 0 ? 1 : i == 1 ? 5 : i == 2 ? 9 : i == 3 ? 13 : i == 4 ? 17 : 21] 
+                + "\n 合計: " + strsplit2[i == 0 ? 3 : i == 1 ? 7 : i == 2 ? 11 : i == 3 ? 15 : i == 4 ? 19 : 23]
+            }
+            );
+
+        var embedBuilder = new EmbedBuilder()
+        {
+            Author = new EmbedAuthorBuilder() { Name = strsplit[2] + " #" + strsplit[0] },
+            ThumbnailUrl = "https://xivapi.com/" + strsplit[1],
+            Color = Color.DarkOrange,
+            Title = "マーケット一覧"
         }
+            .WithFields(list)
+            .WithCurrentTimestamp();
+
+
+        await command.RespondAsync(embed: embedBuilder.Build());
+        
     }
 
-    string searchitem(string item) 
+    string searchitem(string item)
     {
         string url = "https://xivapi.com/search?indexes=item&string=" + item + "&fuzzy=1&language=ja" + key_Token("key");
         WebRequest request = WebRequest.Create(url);
         Stream response_stream = request.GetResponse().GetResponseStream();
         StreamReader reader = new StreamReader(response_stream);
         var xiv_json = JObject.Parse(reader.ReadToEnd());
-
         if ((int)xiv_json["Pagination"]["Results"] == 1)
         {
             var j_id = xiv_json["Results"][0]["ID"];
@@ -127,6 +145,53 @@ class Program
         return null;
 
 
+    }
+
+    string market_world()
+    {
+
+        string url = "https://universalis.app/api/v2/59/36047?listings=10&hq=true";
+        WebRequest request = WebRequest.Create(url);
+        Stream response_stream = request.GetResponse().GetResponseStream();
+        StreamReader reader = new StreamReader(response_stream);
+        var xiv_json = JObject.Parse(reader.ReadToEnd());
+        string list = null;
+        for (int i = 0; i < 6; i++) {
+            var j_pricePerUnit = xiv_json["listings"][0]["pricePerUnit"];   //0,4,8,12,16,20
+            var j_quantity = xiv_json["listings"][0]["quantity"];           //1,5,9,13,17,21
+            var j_hq = xiv_json["listings"][0]["hq"];                       //2,6,10,14,18,22
+            var j_total = xiv_json["listings"][0]["total"];                 //3,7,11,15,19,23
+
+            list = list + j_pricePerUnit + "," + j_quantity + "," + j_hq + "," + j_total + ",";
+        }
+        return list;
+    }
+
+    string market_dc()
+    {
+
+        string url = "https://universalis.app/api/v2/59/36047?listings=10&hq=true";
+        WebRequest request = WebRequest.Create(url);
+        Stream response_stream = request.GetResponse().GetResponseStream();
+        StreamReader reader = new StreamReader(response_stream);
+        var xiv_json = JObject.Parse(reader.ReadToEnd());
+     
+
+        return ";";
+    }
+
+    string market_jp()
+    {
+
+        string url = "https://universalis.app/api/v2/59/36047?listings=10&hq=true";
+        WebRequest request = WebRequest.Create(url);
+        Stream response_stream = request.GetResponse().GetResponseStream();
+        StreamReader reader = new StreamReader(response_stream);
+        var xiv_json = JObject.Parse(reader.ReadToEnd());
+        var j_id = xiv_json["Results"][0]["ID"];
+
+
+        return ";";
     }
 
 }
